c3a149ea6dcbc6a53911cba08f102f2243313d8b
eneace13 <nievesaracil@gmail.com>
2019-11-18 18:52:30 +0100

    Rama privado

--- src/com/aplicacion/Aplicacion.java
+++ src/com/aplicacion/Aplicacion.java
@@ -2,7 +2,13 @@ package com.aplicacion;
 
 
 public class Aplicacion {
-	public static String veredicto (String valor1, String valor2) {
+	/**
+	 * El metodo sirve para comprobar los valores introducidos y darnos un veredicto con respecto a la calificación que esperabamos conseguir y la que hemos conseguido al final.
+	 * @param valor1 donde indicas la calificación deseada.
+	 * @param valor2 donde indicas la calificación real del módulo.
+	 * @return nos devuelve el veredicto después de comparar las notas.
+	 */
+	private static String veredicto (String valor1, String valor2) {
 		if (valor1.equals(valor2)) {
 		    return "Perfecto. Has tenido buen criterio.";
 		}
@@ -11,7 +17,7 @@ public class Aplicacion {
 		}
 		return "Te has confiado. Falta realismo.";
 	}
-	
+
 	public static void main(String[]args) {
 		System.out.println("La calificación de " + args [0] + ":");
 		String veredicto = veredicto(args[1] , args[2]);
